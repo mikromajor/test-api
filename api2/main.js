921f41c9e1833c7e5e04eec3efa7ed783cf95f81
alex.solianyk.ua <alex.solianyk.ua@gmail.com>
2021-09-19 18:16:23 +0300

    added button

--- api2/main.js
+++ api2/main.js
@@ -6,55 +6,35 @@ card.appendChild(ul);
 const loader = document.querySelector('#loader');
 const buttons = document.getElementsByTagName('button');
 const whenLoading = document.getElementById('whenLoading');
-
-const arrSavedImg = [['smallImgUrls'], ['middleImgUrls'], ['largeImgUrls']];
-// {smallImgUrls: [], middleImgUrls:[], largeImgUrls:[]}
-
-// for ... in
-
 const arrButtonNames = [];
-let numBtn;
 
 // fetch
 const getArrObjUsers = () => {
   const arrObjUsers = fetch('https://randomuser.me/api/?results=20')
-    .then((requestOrAnswer) => requestOrAnswer.json())
+    .then((request) => request.json())
     .then((obj) => obj.results)
-    .catch((error) => console.log('I catched error =>', error));
+    .catch((error) => console.log('fetch error =>', error));
   console.log('arrObjUsers -> ', arrObjUsers);
-
   return arrObjUsers;
 };
 
-//
-//creating access to button
-// starting asinch when button clicked
-//
 let but3 = buttons[3].addEventListener('click', () => changeImg('L'));
 let but4 = buttons[4].addEventListener('click', () => changeImg('M'));
 let but5 = buttons[5].addEventListener('click', () => changeImg('S'));
 let but6 = buttons[6].addEventListener('click', () => addColorInCard());
 
-//create button  -0 -1 and call results fetch
 for (let i = 0; i < 3; i++) {
-
   buttons[i].addEventListener('click', (e) => {
-    // numBtn = i;
-
-    // e.target.dataset('gender')//same
-    // e.target.getAttribute('data-gender')// какая кнп нажата
-
     const userGender = e.target.dataset.gender; // <button data-gender="female">...</button>
     loadUsersData(userGender);
   });
 }
+
 function loadUsersData(userGender) {
   clearingContent();
-  // if comming response from server -> starting processing
   getArrObjUsers()
     .then((arrObjUsers) => filterUsers(arrObjUsers, userGender))
-    // .then(filteredUsers => ul.append(...filteredUsers.map(user => creatingUserCard(user)))) //пока проходят здесь процедуры
-    .then((filteredUsers) =>
+        .then((filteredUsers) =>
       filteredUsers.forEach((user) => ul.appendChild(creatingUserCard(user)))
     )
     .then(() => loader.classList.add('loader__hidden')); //запускается это
@@ -71,76 +51,6 @@ function clearingContent() {
     ul.removeChild(ul.firstChild);
   }
 }
-
-// function clearingImgUrls() {
-//   for (let i = arrSavedImg.length - 1; i >= 0; i--) {
-//     while (arrSavedImg[i].length >= 1) {
-//       arrSavedImg[i].pop();
-//     }
-//   }
-// }
-
-
-// function iteratingArrObjUsers(arrObjUsers) {
-//   // for (let i = 0; i < arrObjUsers.length; i++) {
-//   //   filterGenderAndSameImg(arrObjUsers[i]);
-//   // }
-
-
-//   arrObjUsers.forEach((element) => filterGenderAndSameImg(element));
-
-//   console.log(' Amound kard =--> ', ul.childElementCount);
-//   console.log('arr S FOTO -> ', arrSavedImg[2].length);
-//   console.log('arr S FOTO URL-> ', arrSavedImg[2]);
-// }
-
-// let comparing = (str1, str2) => {
-//   return str1.replace(/\D*/gi, '') == str2.replace(/\D*/gi, '');
-// };
-
-// function saveImg(s, m, l) {
-//   arrSavedImg[0].push(s);
-//   arrSavedImg[1].push(m);
-//   arrSavedImg[2].push(l);
-// }
-
-// function filterGenderAndSameImg(objUser) {
-//   switch (numBtn) {
-//     case 0:
-//       if (
-//         objUser.gender == 'female' &&
-//         !arrSavedImg[2].some((ImgUserUrl) => {
-//           return comparing(ImgUserUrl, objUser.picture.large);
-//         })
-//       ) {
-//         saveImg(objUser.picture.thumbnail, objUser.picture.medium, objUser.picture.large);
-//         ul.appendChild(creatingUserCard(objUser));
-//       }
-//       break;
-//     case 1:
-//       if (
-//         objUser.gender == 'male' &&
-//         !arrSavedImg[2].some((ImgUserUrl) => {
-//           return comparing(ImgUserUrl, objUser.picture.large);
-//         })
-//       ) {
-//         saveImg(objUser.picture.small, objUser.picture.midle, objUser.picture.large); // chack -> small, midle
-//         ul.appendChild(creatingUserCard(objUser));
-//       }
-//       break;
-//     case 2:
-//       if (
-//         !arrSavedImg[2].some((ImgUserUrl) => {
-//           return comparing(ImgUserUrl, objUser.picture.large);
-//         })
-//       ) {
-//         saveImg(objUser.picture.small, objUser.picture.midle, objUser.picture.large); // chack -> small, midle
-//         ul.appendChild(creatingUserCard(objUser));
-//       }
-//       break;
-//   }
-// }
-
 function creatingUserCard(user) {
   const imgL = document.createElement('img');
   imgL.src = user.picture.large;
@@ -182,7 +92,7 @@ function creatingUserCard(user) {
   li.classList.add('liCard');
   return li;
 }
-// testing  Timeout on Style Top
+
 const nav__ul = document.getElementById('nav__ul');
 let timerID = setTimeout(() => {
   nav__ul.classList.add('nav__ul'), 3000;
@@ -201,73 +111,9 @@ function changeImg(size) {
       }
   }
 }
-
-// function changeImg(size) {
-//   const imgs = document.getElementsByTagName('img');
-//   console.log(imgs);
-
-//   let urlImg;
-//   let style;
-//   switch (size) {
-//     case 'S':
-//       urlImg = arrSavedImg[0];
-//       style = 'S';
-//       break;
-//     case 'M':
-//       urlImg = arrSavedImg[1];
-//       style = 'M';
-//       break;
-//     case 'L':
-//       urlImg = arrSavedImg[2];
-//       style = 'L';
-//       break;
-//   }
-//   console.log(urlImg);
-//   for (let i = 0; i < imgs.length; i++) {
-//     imgs[i].src = urlImg[i];
-
-//     imgs[i].classList.remove('L');
-//     imgs[i].classList.remove('M');
-//     imgs[i].classList.remove('S');
-//     imgs[i].classList.add(style);
-//   }
-// }
-
 function addColorInCard() {
   const divs_cardBox = document.querySelectorAll('div.cardBox');
   for (let i = 0; i < divs_cardBox.length; i++) {
     divs_cardBox[i].classList.add('colored');
   }
-  // let i;
-  // const arr2arrsM_F = findGender();
-  // for (i = 0; i < arr2arrsM_F[0].length; i++) {
-  //   arr2arrsM_F[0][i].classList.add('male');
-  // }
-  // for (i = 0; i < arr2arrsM_F[1].length; i++) {
-  //   arr2arrsM_F[1][i].classList.add('female');
-  // }
 }
-
-// function findGender(gender) {
-//   const arrMales = [];
-//   const arrFemales = [];
-//   const arrMalesAndFemales = [];
-//   const divs_cardBox = document.querySelectorAll('div.cardBox');
-
-//   for (let i = 0; i < divs_cardBox.length; i++) {
-//     if (divs_cardBox[i].classList.contains('cardBox--male')) {
-//       arrMales.push(divs_cardBox[i]);
-//     } else if (divs_cardBox[i].classList.contains('cardBox--female')) {
-//       arrFemales.push(divs_cardBox[i]);
-//     }
-//   }
-//   if (gender == 'X') {
-//     return arrMales;
-//   } else if (gender == 'Y') {
-//     return arrFemales;
-//   } else {
-//     arrMalesAndFemales.push(arrMales);
-//     arrMalesAndFemales.push(arrFemales);
-//     return arrMalesAndFemales;
-//   }
-// }
--- api3_covid/main.js
+++ api3_covid/main.js
@@ -1,12 +1,14 @@
 const select = document.getElementById("countriesList");
 let ignoreFirstClick = 0;
-let url;
-//  fetch get country names
+const countryCovidInf = []; //Covid for statistic calculation
+let countryUrl;
+
+//  fetch 1 get country names
 const getCountries = () => {
   const countries = fetch("https://api.covid19api.com/countries")
     .then((request) => request.json())
     .then((data) => {
-      console.log("obj from fetch 1 =>", data);
+      console.log("fetch 1 country names =>", data);
       return data;
     })
     .catch((error) => console.log("error in 1-st fetch =>", error));
@@ -22,56 +24,121 @@ function addCountriesList() {
         option.textContent = obj.Country;
         select.appendChild(option);
       })
-    ) // searching option.selected to call startCreateContent(country)
-    // (alternative -> for...in)
+    ) // searching option.selected to call pullCovidInf(country)
     .then(() => {
       select.addEventListener("click", () => {
-        let selected = Array.from(select.options)
-          .filter((option) => option.selected)
-          .map((option) => option.value);
-        startCreateContent(selected[0]);
+        let select = document.getElementById("countriesList");
+        let selected = select.options[select.selectedIndex];
+        pullCovidInf(selected.value);
       });
     });
 }
-// check loading window
-if (document.readyState === "loading") {
-  document.addEventListener("DOMContentLoaded", addCountriesList());
-} else {
-  addCountriesList(); //  <----= START
-}
-//????????????????????????????????????????
+
+// fetch 2
 function getCovidInf() {
-  console.log("in fetch 2 url looks like-> ", url);
-  const covidInf = fetch(url)
+  const covidInf = fetch(countryUrl)
     .then((request) => request.json())
     .then((covidInf) => {
-      console.log("obj from fetch 2 =>", covidInf);
-      return covidInf[0];
+      console.log("fetch 2 - covid inf from country =>", covidInf);
+      return covidInf;
     })
     .catch((error) => console.log("error in 2-nd fetch =>", error));
   return covidInf;
 }
 
-function startCreateContent(country) {
+function pullCovidInf(country) {
   // for ignore first click
   if (country == ignoreFirstClick) {
     return undefined;
   }
   ignoreFirstClick = country;
-  url = "https://api.covid19api.com/country/" + country;
-  //
-  getCovidInf().then((objInf) => {
-    const content = document.getElementById("content");
-    content.removeChild(content.firstChild);
-    // create content with
-    const h2 = document.createElement("h2");
-    h2.textContent = "Country - " + objInf.Country;
-    const p1 = document.createElement("p");
-    p1.textContent = "DEADTH";
-    const p2 = document.createElement("p");
 
-    content.appendChild(h2);
-    content.appendChild(p1);
-    content.appendChild(p2);
+  //
+  countryUrl = "https://api.covid19api.com/country/" + country;
+
+  getCovidInf().then((Covid) => {
+    // checking country  data for availability inform cov
+    const content = clearContent();
+    const h2 = document.createElement("h2");
+    if (
+      Covid[Covid.length - 1].Active == 0 ||
+      Covid[Covid.length - 31].Active == 0
+    ) {
+      h2.textContent =
+        "Server hasn't information about covid-19 in this country";
+      content.appendChild(h2);
+    } else {
+      h2.textContent = "Country - " + Covid[Covid.length - 1].Country;
+
+      const p0 = document.createElement("p");
+      p0.textContent = `Date - ${Covid[Covid.length - 1].Date}`;
+
+      const p1 = document.createElement("p");
+      p1.textContent = `Amount of infected - ${Covid[Covid.length - 1].Active}`;
+
+      const p2 = document.createElement("p");
+      p2.textContent = `Amount deaths - ${Covid[Covid.length - 1].Deaths}`;
+
+      content.appendChild(h2);
+      content.appendChild(p0);
+      content.appendChild(p1);
+      content.appendChild(p2);
+      content.classList.add("content");
+
+      //create BUTTON
+      const week = document.createElement("button");
+      week.textContent = "Statistics for last week";
+      week.addEventListener("click", () => {
+        addStatistic(Covid[Covid.length - 1], Covid[Covid.length - 8], "week");
+      });
+      const month = document.createElement("button");
+      month.textContent = "Statistics for last month";
+      month.addEventListener("click", () => {
+        addStatistic(
+          Covid[Covid.length - 1],
+          Covid[Covid.length - 31],
+          "month"
+        );
+      });
+      content.appendChild(week);
+      content.appendChild(month);
+    }
   });
+}
+
+function clearContent() {
+  const content = document.getElementById("content");
+  while (content.firstChild) {
+    content.removeChild(content.firstChild);
+  }
+  return content;
+}
+
+function addStatistic(today, dayAgo, time) {
+  const cont = clearContent();
+  const deaths = document.createElement("p");
+  deaths.textContent = "People DEATHS -" + (today.Deaths - dayAgo.Deaths);
+  const confirmed = document.createElement("p");
+  confirmed.textContent =
+    "People CONFIRMED -" + (today.Confirmed - dayAgo.Confirmed);
+  const active = document.createElement("p");
+  active.textContent = "People ACTIVE -" + (today.Active - dayAgo.Active);
+  const Time = document.createElement("h2");
+  Time.textContent = `For last ${time}`;
+  const buttonReturn = document.createElement("button");
+  buttonReturn.textContent = "Return";
+  buttonReturn.addEventListener("click", () => {});
+  cont.appendChild(Time);
+  cont.appendChild(active);
+  cont.appendChild(confirmed);
+  cont.appendChild(deaths);
+  cont.appendChild(buttonReturn);
+}
+
+//    S   T   A   R    T
+//  <---= check loading window and  START
+if (document.readyState === "loading") {
+  document.addEventListener("DOMContentLoaded", addCountriesList());
+} else {
+  addCountriesList();
 }
